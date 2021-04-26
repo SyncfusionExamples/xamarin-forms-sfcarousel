ac28c2ffed0210094cddcdc083fea8bb4423ab3b
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 11:02:58 +0530

    Getting started sample added for carousel

---
+++ GettingStarted/GettingStarted/Model/CarouselModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettingStarted
+{
+    public class CarouselModel
+    {
+        public CarouselModel(string imagestr)
+        {
+            Image = imagestr;
+        }
+        private string _image;
+
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+}
---
+++ GettingStarted/GettingStarted/ViewModel/CarouselViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public class CarouselViewModel
+    {
+        public CarouselViewModel()
+        {
+            ImageCollection.Add(new CarouselModel("image1.png"));
+            ImageCollection.Add(new CarouselModel("image2.png"));
+            ImageCollection.Add(new CarouselModel("image3.png"));
+            ImageCollection.Add(new CarouselModel("image4.png"));
+            ImageCollection.Add(new CarouselModel("image5.png"));
+        }
+        private List<CarouselModel> imageCollection = new List<CarouselModel>();
+        public List<CarouselModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+    }
+}
